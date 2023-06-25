e1b6e9606f33c4723c04b2082e728bc96c05f29a
Rodrigo Morais <rmoraisu@protonmail.com>
2023-06-25 12:50:28 -0500

    closes #7, closes #6, related to #1

--- canvy/transform.ts
+++ canvy/transform.ts
@@ -4,4 +4,27 @@ export class CanvyTransform {
   scale(this: Canvy, x: number, y: number) {
     this.ctx.scale(x, y);
   }
+
+  translate(this: Canvy, x: number, y: number) {
+    this.ctx.translate(x, y);
+  }
+
+  transform(
+    this: Canvy,
+    horizontalScale: number,
+    horizontalSkew: number,
+    verticalSkew: number,
+    verticalScale: number,
+    horizontalMove: number,
+    verticalMove: number
+  ) {
+    this.ctx.transform(
+      horizontalScale,
+      horizontalSkew,
+      verticalSkew,
+      verticalScale,
+      horizontalMove,
+      verticalMove
+    );
+  }
 }
